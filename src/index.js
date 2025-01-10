5ed9c8d218ba1a2e79ec1bb5251ae05456cbed3d
srikrishnastrive <srikrishnamuddangala@gmail.com>
2025-01-10 05:53:16 +0530

    git added initial project

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { PORT } from './config/serverConfig.js';
+import cors from 'cors';
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    return res.json({message:'pong'});
+})
+
+app.listen(PORT,()=>{
+    console.log(`sever start at ${PORT}`);
+});
